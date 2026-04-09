d071145da2953d61e31b13ca161422f8056655be
shahSpider <aq.syed007@gmail.com>
2026-04-09 11:51:12 +0500

    cleanup: remove pycache and add migrations

---
+++ alembic/versions/abdd36e701e9_initiate_migration.py
@@ -0,0 +1,28 @@
+"""initiate migration
+
+Revision ID: abdd36e701e9
+Revises: 
+Create Date: 2026-04-01 12:37:37.890403
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'abdd36e701e9'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
